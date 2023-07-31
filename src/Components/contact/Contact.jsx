f9f8a5c3645ebe66e126ddc0f7452d0bfbbcbcc8
Deep Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2023-07-31 18:46:32 +0530

    intro page, portfolio page completed, favicon changed, started working with contact us page

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,9 +1,35 @@
-import './contact.scss'
+import "./contact.scss";
 
 export default function Contact() {
   return (
-    <div className='contact' id='contact'>Contact</div>
-  )
+    <div className="contact" id="contact">
+      <div className="container">
+        <h3>Get in touch today!</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <form className="formContainer" action="https://formspree.io/f/xdoragno" method="POST">
+      
+          <label htmlFor="Name">Name</label>
+          <div>
+            <input type="text" name="Name" />
+          </div>
+          <label htmlFor="Subject ">Subject</label>
+          <div>
+            <input type="text" name="Subject " />
+          </div>
+          <label htmlFor="Work Mail">Work Mail</label>
+          <div>
+            <input type="email" name="Work Mail" />
+          </div>
+          <label htmlFor="name">Message</label>
+          <div>
+            <textarea name="name" placeholder="Type Your Message here" />
+          </div>
+
+          <div>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
-
-
--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -1,32 +1,36 @@
-import { useEffect, useRef } from 'react';
-import './intro.scss';
-
+import { useEffect, useRef } from "react";
+import "./intro.scss";
 
 export default function Intro() {
- const profileimg=useRef();
- 
-  useEffect(()=>{
-    profileimg.current.style.left="0%";
-  },[])
-  
+  const profileimg = useRef();
+
+  useEffect(() => {
+    profileimg.current.style.left = "0%";
+  }, []);
+
   return (
-    <div className='intro' id="intro">
+    <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img ref={profileimg} src='assets/DeepLajpal.Webp' alt='profile photo'></img>
+          <img ref={profileimg} src="assets/DeepLajpal.png" alt="profile"></img>
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Deep Lajpal</h1>
-          <h3><span>Full-Stack Developer</span></h3>
+          <h3>
+            <span>Full-Stack Developer</span>
+          </h3>
         </div>
         <a href="#portfolio">
-          <img src="assets/down.webp" alt='down arrow image for portfolio
-          '></img>
+          <img
+            src="assets/down.webp"
+            alt="down arrow for portfolio
+          "
+          ></img>
         </a>
       </div>
     </div>
-  )
+  );
 }
